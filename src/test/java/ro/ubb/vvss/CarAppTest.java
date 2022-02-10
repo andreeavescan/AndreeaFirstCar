b0eb167b36f72f1f66047442330a531800f2e3f2
osboxes.org <bogdan.iudean@gmail.com>
2022-02-10 03:44:45 -0500

    Initial commit

---
+++ src/test/java/ro/ubb/vvss/CarAppTest.java
@@ -0,0 +1,12 @@
+package ro.ubb.vvss;
+
+import static org.junit.Assert.assertTrue;
+
+public class CarAppTest {
+    @org.junit.Test
+    public void test1()
+    {
+        System.out.print("Test 1 executing...");
+        assertTrue(true);
+    }
+}
